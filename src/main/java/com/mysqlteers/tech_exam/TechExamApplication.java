d2162f2dab6db9a6a233ae26ea8692642fcea4bf
emilnorsker <emilgraaenorsker@gmail.com>
2020-11-06 11:36:08 +0100

    Created spring project

---
+++ src/main/java/com/mysqlteers/tech_exam/TechExamApplication.java
@@ -0,0 +1,14 @@
+package com.mysqlteers.tech_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechExamApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TechExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mysqlteers/tech_exam/TechExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysqlteers.tech_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
