d9ee3f487187df36b9c306396cf7b39b67f4f260
caerKEA <58211711+caerKEA@users.noreply.github.com>
2020-11-06 15:39:12 +0100

    Add new person method

--- src/main/java/com/mysqlteers/tech_exam/HomeController.java
+++ src/main/java/com/mysqlteers/tech_exam/HomeController.java
@@ -1,14 +1,17 @@
 package com.mysqlteers.tech_exam;
 
+import com.mysqlteers.tech_exam.Model.Person;
 import com.mysqlteers.tech_exam.repository.PersonRepository;
 import com.mysqlteers.tech_exam.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller()
+@Controller
 public class HomeController {
     private PersonService personService;
 
@@ -16,9 +19,15 @@ public class HomeController {
         this.personService = personService;
     }
 
-    @GetMapping
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("persons", personService.findAll());
         return "index";
+    }
+
+    @PostMapping("/createPerson")
+    public String createPerson(@ModelAttribute Person newPerson) {
+        personService.save(newPerson);
+        return "redirect:/";
     }
 }
