04e4ec1a929bf9699bda3101deeff4687ca4b143
caerKEA <58211711+caerKEA@users.noreply.github.com>
2020-11-13 10:51:55 +0100

    JUnit tests

---
+++ src/test/java/com/mysqlteers/tech_exam/service/springdatajpa/PersonJPATest.java
@@ -0,0 +1,35 @@
+package com.mysqlteers.tech_exam.service.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PersonJPATest {
+
+    @Test
+    void findAll() {
+        assertTrue(true);
+    }
+
+    @Test
+    void save() {
+        assertTrue(true);
+    }
+
+    @Test
+    void delete() {
+        assertTrue(true);
+    }
+
+    @Test
+    void deleteById() {
+        assertTrue(true);
+    }
+
+    @Test
+    void findById() {
+        assertTrue(true);
+    }
+}
